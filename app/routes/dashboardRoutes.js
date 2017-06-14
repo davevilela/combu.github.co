4ee23ff32553a7cfd9b5354a00b5dbb80a9a7bac
davifalta <davivilelaa@gmail.com>
2017-06-14 15:58:38 -0300

    commit 2017-06-14
    -New dependency: Socket.io
    -process.env.PORT set for hosting

--- app/routes/dashboardRoutes.js
+++ app/routes/dashboardRoutes.js
@@ -32,6 +32,7 @@ module.exports = function(app){
         })
         .post(api.insert);
 
+    
 };
 
 
--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 var app = require('./config/express')();
+var http = require('http').Server(app);
+var io = require('socket.io');
 
-app.listen(3000, function(){
-    console.log('Server da combu rodando');
-});
+var porta = process.env.PORT || 3000;
+
+var server = http.listen(porta, function(){
+    
+    var host = server.address().address;
+    var port = server.address().port;
+    
+    console.log('Combu app listening at http://%s:%s', host, port);
+})
